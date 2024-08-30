52dba0d848f2f116ec1fa05988203b110538d816
Nico Reinders <Reinders@institut2b.physik.rwth-aachen.de>
2024-08-30 16:25:51 +0200

    start nblip evaluation

--- evaluate_with_exdata.py
+++ evaluate_with_exdata.py
@@ -5,20 +5,18 @@ import pickle
 import os
 from torch.utils.data import DataLoader
 from test_lib import schmitt_trigger
+from unet_model import UNet
+import torch
 
+print('GPU available: ', torch.cuda.is_available())
 
-new_array = np.load('newarray.npy')
-prediction_class, thresh_lower, thresh_upper = schmitt_trigger(new_traces_array=new_array, full_output=True)
-
-print(thresh_lower)
-print(thresh_upper)
-
-
-
-
-
-#%%
-
+# Set device to GPU if available, else CPUs
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if device == torch.device('cuda'):
+    num_workers = 4
+else: 
+    num_workers = 1
+    #mpl.use('Qt5Agg')
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 model_dir = os.path.join(current_dir, 'unet_params_ex')
@@ -30,7 +28,8 @@ def unpickle_loader(name, shuffle=True):
         data['dataset'],
         batch_size=data['batch_size'],
         shuffle=shuffle,
-        num_workers=data['num_workers'],
+        #num_workers=data['num_workers'],
+        num_workers=0,
         collate_fn=data['collate_fn']
         )
     return new_dataloader
@@ -39,4 +38,31 @@ def unpickle_loader(name, shuffle=True):
 train_loader = unpickle_loader('train_loader')
 val_loader = unpickle_loader('test_loader')
 test_loader = unpickle_loader('test_loader', shuffle=False)
+
+
+
+print('Loading model')
+model = UNet()
+
+model_dir = os.path.join(current_dir, 'unet_params_ex')
+state_dict_name = 'model_weights'  
+state_dict_path = os.path.join(model_dir, '{}.pth'.format(state_dict_name))  
+model.load_state_dict(torch.load(state_dict_path, weights_only=True))  
+model.eval()
+print('model loaded')
+
+
+
+with torch.no_grad():  
+    for x, y in val_loader:
+        x = x.to(device)
+        y = y.to(device)
+        
+        decoded_test_data = model(x)
+        m = torch.nn.Softmax(dim=1)
+        decoded_test_data = m(decoded_test_data)
+        decoded_test_data = decoded_test_data.cpu().numpy()  # Get model output for visualization
+        #prediction_class = decoded_test_data.argmax(axis=1)
+        logits_array = np.vstack((logits_array, decoded_test_data[:, 1, :]))
+        
 
--- playing_with_noise.py
+++ playing_with_noise.py
@@ -11,17 +11,18 @@ from scipy.signal import welch
 plt.switch_backend('TkAgg') 
 
 #noise_path = '//serveri2a/Transfer/Nico/01.Data/Elzerman/545_1.9T_pg13_vs_tc.hdf5'
+# real_trace_path = '//serveri2a/Transfer/Nico/01.Data/Elzerman/494_2_3T_elzermann_test.hdf5'
 real_trace_path = '//serveri2a/Transfer/Nico/01.Data/Elzerman/492_2_5T_elzermann_ - Copy.hdf5'
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-trace_dir = os.path.join(current_dir, 'traces')
+trace_dir = os.path.join(current_dir, 'sim_traces')
 
 tick = time.perf_counter()
 print('Loading data...')
 script_dir = os.path.dirname(os.path.abspath(__file__))
-if not os.path.exists(trace_dir):
-    os.makedirs(wdir=trace_dir)
+# if not os.path.exists(trace_dir):
+#     os.makedirs(wdir=trace_dir)
 #hdf5Data_noise = ElzerData(wdir=trace_dir)
 #hdf5Data_noise.set_path(noise_path)
 
@@ -40,18 +41,17 @@ hdf5Data_traces.set_path(real_trace_path)
 hdf5Data_traces.set_filename()
 hdf5Data_traces.set_traces()
 
-real_traces = hdf5Data_traces.traces
-fig, ax = plt.subplots(1, 1)
-ax.plot(real_traces[46][78])
+# real_traces = hdf5Data_traces.traces
+# fig, ax = plt.subplots(1, 1)
+# ax.plot(real_traces[46][78])
 
 
 hdf5Data_traces.set_traces_dt() # self.data.set_traces_dt()
-traces_dt = hdf5Data_traces.traces_dt
-traces = hdf5Data_traces.traces #self.data.traces
-print('Measured traces: ', traces.shape)  
+# traces_dt = hdf5Data_traces.traces_dt
+# traces = hdf5Data_traces.traces #self.data.traces
+# print('Measured traces: ', traces.shape)  
 
-
-file_name = 'sliced_traces_small'  
+file_name = 'sliced_traces_ordered'  
 hdf5Data_traces.create_new_file_only_traces(file_name)
 #hdf5Data_traces.cut_traces()
 
@@ -62,55 +62,55 @@ hdf5_file_path = os.path.join(trace_dir, '{}.hdf5'.format(file_name))
 #     sliced_traces = np.array([file[key] for key in all_keys],dtype=np.float32)  
 #     print('Sliced measured traces: ', sliced_traces.shape)  
 sliced_traces = np.array(hdf5Data_traces.read_traces)
-whole_traces = np.array(hdf5Data_traces.padded_arrays)
+# whole_traces = np.array(hdf5Data_traces.padded_arrays)
 print('Sliced measured traces: ', sliced_traces.shape)  
-print('Sliced whole measured traces: ', whole_traces.shape)  
+# print('Sliced whole measured traces: ', whole_traces.shape)  
 
 
 #traces_time_axis = traces_dt * np.arange(0, len(noise_traces[0][0]))
 
 
-file_name = 'sim_read_traces_train'  
-hdf5_file_path = os.path.join(trace_dir, '{}.hdf5'.format(file_name))   
+# file_name = 'sim_read_traces_train'  
+# hdf5_file_path = os.path.join(trace_dir, '{}.hdf5'.format(file_name))   
 
 # with h5py.File(hdf5_file_path, 'r') as file:  
 #     all_keys = file.keys()  
 #     sim_traces = np.array([file[key] for key in all_keys], dtype=np.float32)  
 #     print('Simulated read traces: ', sim_traces.shape)  
 
-for i, trace in enumerate(whole_traces):
-    fig, ax = plt.subplots(1, 1)
-    ax.plot(trace)
-    fig.suptitle(f'Trace {i}') 
-    mng = plt.get_current_fig_manager()
-    mng.window.state('zoomed')
+# for i, trace in enumerate(whole_traces):
+#     fig, ax = plt.subplots(1, 1)
+#     ax.plot(trace)
+#     fig.suptitle(f'Trace {i}') 
+#     mng = plt.get_current_fig_manager()
+#     mng.window.state('zoomed')
 
-    plt.show(block=True)
+#     plt.show(block=True)
  
-#%%
-for i_sim in np.random.randint(0, len(sim_traces), 10): 
-    i_noise = np.random.randint(noise_traces.shape[0])
-    j_noise = np.random.randint(noise_traces.shape[1])
+# #%%
+# for i_sim in np.random.randint(0, len(sim_traces), 10): 
+#     i_noise = np.random.randint(noise_traces.shape[0])
+#     j_noise = np.random.randint(noise_traces.shape[1])
     
-    i_real = np.random.randint(sliced_traces.shape[0])
+#     i_real = np.random.randint(sliced_traces.shape[0])
     
-    sim_trace = sim_traces[i_sim]
-    noise_trace = noise_traces[i_noise, j_noise]
-    real_trace = sliced_traces[i_real]
+#     sim_trace = sim_traces[i_sim]
+#     noise_trace = noise_traces[i_noise, j_noise]
+#     real_trace = sliced_traces[i_real]
     
-    start = np.random.randint(0, len(noise_trace)-len(sim_trace))
-    stop = start + len(sim_trace)
-    noise = noise_trace[start:stop]
-    noise -= np.mean(noise)
-    sim_noise_trace = sim_trace + 20*noise
+#     start = np.random.randint(0, len(noise_trace)-len(sim_trace))
+#     stop = start + len(sim_trace)
+#     noise = noise_trace[start:stop]
+#     noise -= np.mean(noise)
+#     sim_noise_trace = sim_trace + 20*noise
 
 
 
     
-    fig, ax = plt.subplots(3, 1, figsize=(12, 6))
-    ax[0].plot(sim_trace)
-    ax[1].plot(sim_noise_trace)
-    ax[2].plot(real_trace)
-#    ax[3].plot(whole_traces[i_real])
-    plt.show(block=True)
+#     fig, ax = plt.subplots(3, 1, figsize=(12, 6))
+#     ax[0].plot(sim_trace)
+#     ax[1].plot(sim_noise_trace)
+#     ax[2].plot(real_trace)
+# #    ax[3].plot(whole_traces[i_real])
+#     plt.show(block=True)
 
